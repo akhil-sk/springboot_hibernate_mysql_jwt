a51f41c6250f68eefb54b8c4571c60637e1c35c1
91989 <akhil.s.kamath@gmail.com>
2020-07-19 21:28:46 +0530

    project commit

---
+++ checklist/src/main/java/com/checklist/checklist/ChecklistApplication.java
@@ -0,0 +1,28 @@
+package com.checklist.checklist;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = { 
+		ChecklistApplication.class,
+		Jsr310JpaConverters.class 
+})
+public class ChecklistApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistApplication.class, args);
+	}
+
+}
---
+++ checklist/src/main/java/com/checklist/checklist/model/RoleName.java
@@ -0,0 +1,8 @@
+package com.checklist.checklist.model;
+
+public enum RoleName {
+	
+	ROLE_USER,
+    ROLE_ADMIN
+
+}
